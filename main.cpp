9ccc1cc73bdfa3cec57f76ba46ad8659c8a7ec4b
Dan Miller <dan@dmiller.dev>
2020-08-12 15:17:30 -0400

    recurse through directories, build with toast

--- main.cpp
+++ main.cpp
@@ -2,13 +2,30 @@
 #include <fstream>
 #include <iostream>
 
+// TODO(dmiller): combine all recursively
+// TODO(dmiller): support `set noparent`
+// TODO(dmiller): write some tests maybe
+
 namespace fs = std::filesystem;
 
 fs::path owner_path(fs::path p) { return p / "OWNERS"; }
 
 bool has_owner_file(fs::path p) { return fs::exists(owner_path(p)); }
 
-int main(int argc, char *argv[]) {
+fs::path find_owners_file_path(fs::path p) {
+  std::cout << "Checking: " << p << std::endl;
+  if (has_owner_file(p)) {
+    return owner_path(p);
+  }
+
+  if (p == p.root_path()) {
+    throw "reached root path and no OWNERS file was found";
+  }
+
+  return find_owners_file_path(p.parent_path());
+}
+
+int main(int argc, char* argv[]) {
   if (argc != 2) {
     std::cout << "must be called with exactly one argument: the path to check "
                  "for OWNERS"
@@ -18,9 +35,8 @@ int main(int argc, char *argv[]) {
 
   auto path = argv[1];
   fs::path p = path;
-  std::cout << "Checking: " << p << std::endl;
-  auto has_file = has_owner_file(p);
-  if (has_file) {
+  try {
+    auto owners_path = find_owners_file_path(fs::absolute(p));
     std::ifstream file(owner_path(p));
     if (file.is_open()) {
       std::string line;
@@ -29,9 +45,7 @@ int main(int argc, char *argv[]) {
       }
       file.close();
     }
-  } else {
-    // eventually we should recurse, but for now let's just error here
-    std::cout << "no OWNERS file found" << std::endl;
-    return -1;
+  } catch (const char* e) {
+    std::cout << "Error finding OWNERS file: " << e << std::endl;
   }
 }
