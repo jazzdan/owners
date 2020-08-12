e2d81ab55c22805f8a55e412433e6c7939f5235b
Dan Miller <dan@dmiller.dev>
2020-08-11 21:29:35 -0400

    correctly check for owners file

--- main.cpp
+++ main.cpp
@@ -1,23 +1,25 @@
 #include <filesystem>
+#include <fstream>
 #include <iostream>
 
 namespace fs = std::filesystem;
 
-bool has_owner_file(fs::path p) { return fs::exists(p); }
+bool has_owner_file(fs::path p) { return fs::exists(p / "OWNERS"); }
 
 int main(int argc, char *argv[]) {
   if (argc != 2) {
     std::cout << "must be called with exactly one argument: the path to check "
-                 "for OWNERs"
+                 "for OWNERS"
               << std::endl;
     return -1;
   }
+
   auto path = argv[1];
   fs::path p = path;
   std::cout << "Checking: " << p << std::endl;
   auto has_file = has_owner_file(p);
   if (has_file) {
-    // read it
+    // TODO read the file
   } else {
     std::cout << "no OWNERS file found" << std::endl;
     return -1;
