4a61ba5f1905c9a0386ddcdcc3cd348e52dd9236
Dan Miller <dan@dmiller.dev>
2020-08-12 15:32:42 -0400

    use std namespace

--- main.cpp
+++ main.cpp
@@ -8,13 +8,14 @@
 // TODO(dmiller): write some tests maybe
 
 namespace fs = std::filesystem;
+using namespace std;
 
 fs::path owner_path(fs::path p) { return p / "OWNERS"; }
 
 bool has_owner_file(fs::path p) { return fs::exists(owner_path(p)); }
 
-fs::path traverse_owners_files(fs::path p, std::vector<std::string>& owners) {
-  std::cout << "Checking: " << p << std::endl;
+fs::path traverse_owners_files(fs::path p, vector<string>& owners) {
+  cout << "Checking: " << p << endl;
   if (has_owner_file(p)) {
     return owner_path(p);
   }
@@ -28,26 +29,26 @@ fs::path traverse_owners_files(fs::path p, std::vector<std::string>& owners) {
 
 int main(int argc, char* argv[]) {
   if (argc != 2) {
-    std::cout << "must be called with exactly one argument: the path to check "
-                 "for OWNERS"
-              << std::endl;
+    cout << "must be called with exactly one argument: the path to check "
+            "for OWNERS"
+         << endl;
     return -1;
   }
 
   auto path = argv[1];
   fs::path p = path;
-  std::vector<std::string> owners;
+  vector<string> owners;
   try {
     auto owners_path = traverse_owners_files(fs::absolute(p), owners);
-    std::ifstream file(owner_path(p));
+    ifstream file(owner_path(p));
     if (file.is_open()) {
-      std::string line;
-      while (std::getline(file, line)) {
-        std::cout << line << std::endl;
+      string line;
+      while (getline(file, line)) {
+        cout << line << endl;
       }
       file.close();
     }
   } catch (const char* e) {
-    std::cout << "Error finding OWNERS file: " << e << std::endl;
+    cout << "Error finding OWNERS file: " << e << endl;
   }
 }
